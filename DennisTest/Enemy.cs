50d38cf7ac825ccc36a136224d1e356f1b69ad38
DennisLeoRosdahl <143614047+DennisLeoRosdahl@users.noreply.github.com>
2024-03-21 09:47:08 +0100

    Create Enemy.cs
    Added variables

---
+++ DennisTest/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DennisTest
+{
+
+    internal class Enemy
+    {
+        int enemyHealth;
+        string enemyName;
+        bool isAlive;
+        //Test comment
+    }
+}
