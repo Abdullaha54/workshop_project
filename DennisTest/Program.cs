bb33065984c0340a2407747cb500f7d76e3ac3b6
DennisLeoRosdahl <143614047+DennisLeoRosdahl@users.noreply.github.com>
2024-03-21 09:41:23 +0100

    Added Project
    DennisTest added

---
+++ DennisTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DennisTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
