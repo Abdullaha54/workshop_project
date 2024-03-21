c6ec2693d3916be47659fd1e84f0f2c46259147d
AllanKh <143695520+AllanKh@users.noreply.github.com>
2024-03-21 09:48:43 +0100

    Created Enemy.cs

---
+++ DennisTest/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DennisTest
+{
+    internal class Enemy
+    {
+        public void enemy()
+        {
+            Console.WriteLine("Hello Enemy!");
+        }
+    }
+}
